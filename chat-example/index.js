6cf1b91bc54c7353e450a02e098b321c92209895
Sam <berg1744@umn.edu>
2017-07-04 18:28:49 -0500

    Server now receives messages

--- chat-example/index.js
+++ chat-example/index.js
@@ -1,12 +1,25 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var port_number = '9001';
 
 app.get('/', function(request, response){
-	response.send('<h1>Hello, Butter and Express!</h1>');
+	response.sendFile(__dirname + '/index.html');
 });
 
-port = '9001';
+io.on('connection', function(socket){
+	console.log('Oh look, a user connected!');
 
-http.listen(port, function(){
-	console.log('listening on *:'+port);
+	socket.on('chat message', function(message){
+		console.log('\tmessage:' + message);
+	});
+
+	socket.on('disconnect', function(){
+		console.log('Zey disconnected!');
+	});
+});
+
+http.listen(port_number, function(){
+	console.log('listening on *:'+port_number);
 });
